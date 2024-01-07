7d2b34745d762fb9b4da51f699f8b27bdc22e0c6
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-01-07 20:24:38 +0100

    Add Split Bill project setup

---
+++ Split Bill/Split Bill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Split Bill
+//
+//  Created by Michal Pietrzak on 07/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
