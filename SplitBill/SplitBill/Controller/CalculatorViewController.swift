75a30089a418af4b15b7e7f7ac51fda58b58d5ae
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-01-12 16:35:30 +0100

    Update buttons -> color, font, Button Style: default

--- SplitBill/SplitBill/Controller/CalculatorViewController.swift
+++ SplitBill/SplitBill/Controller/CalculatorViewController.swift
@@ -40,19 +40,31 @@ class CalculatorViewController: UIViewController {
         
         view.backgroundColor = Colors.lightGray
         
-        billTotalLbl.textColor  = Colors.black
-        billTextField.textColor = Colors.darkBlue
-        selectTipLbl.textColor  = Colors.black
-        splitLbl.textColor      = Colors.black
-        splitNumberLbl.textColor = Colors.darkBlue
+        billTotalLbl.textColor       = Colors.black
+        billTextField.textColor      = Colors.darkBlue
+        selectTipLbl.textColor       = Colors.black
+        splitLbl.textColor           = Colors.black
+        splitNumberLbl.textColor     = Colors.darkBlue
+        zeroPctBtn.tintColor         = Colors.darkBlue
+        tenPctBtn.tintColor          = Colors.darkBlue
+        TwentyPctBtn.tintColor       = Colors.darkBlue
+        calculateBtn.tintColor       = Colors.lightGray
+        calculateBtn.backgroundColor = Colors.darkBlue
         
-        billTotalLbl.font   = UIFont(name: Fonts.sfProRoundedBold, size: 20)
-        selectTipLbl.font   = UIFont(name: Fonts.sfProRoundedBold, size: 20)
-        splitLbl.font       = UIFont(name: Fonts.sfProRoundedBold, size: 20)
-        billTextField.font  = UIFont(name: Fonts.sfProRoundedBold, size: 17)
-        splitNumberLbl.font = UIFont(name: Fonts.sfProRoundedBold, size: 17)
+        billTotalLbl.font             = UIFont(name: Fonts.sfProRoundedBold, size: 20)
+        selectTipLbl.font             = UIFont(name: Fonts.sfProRoundedBold, size: 20)
+        splitLbl.font                 = UIFont(name: Fonts.sfProRoundedBold, size: 20)
+        billTextField.font            = UIFont(name: Fonts.sfProRoundedBold, size: 17)
+        splitNumberLbl.font           = UIFont(name: Fonts.sfProRoundedBold, size: 17)
+        zeroPctBtn.titleLabel?.font   = UIFont(name: Fonts.sfProRoundedBold, size: 17)
+        tenPctBtn.titleLabel?.font    = UIFont(name: Fonts.sfProRoundedBold, size: 17)
+        TwentyPctBtn.titleLabel?.font = UIFont(name: Fonts.sfProRoundedBold, size: 17)
+        calculateBtn.titleLabel?.font = UIFont(name: Fonts.sfProRoundedBold, size: 20)
         
         billTextField.layer.cornerRadius = 5
+        calculateBtn.layer.cornerRadius  = 5
+        
+        
         
     }
 }
