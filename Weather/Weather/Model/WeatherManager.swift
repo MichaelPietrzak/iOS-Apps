81d608bd8d4b6d602fefcc51c4dc6565706fbda6
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-02-08 11:00:59 +0100

    feat: add API key + units in urlString

--- Weather/Weather/Model/WeatherManager.swift
+++ Weather/Weather/Model/WeatherManager.swift
@@ -10,11 +10,11 @@ import Foundation
 struct WeatherManager {
     
     let secureAPIkey = ProcessInfo.processInfo.environment["WEATHER_API_KEY"] ?? "API Key error!"
-    
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid="
+    let units = "&units=metric"
     
     func fetchWeather(cityName: String) {
-        let urlString = "\(weatherURL)&q=\(cityName)"
+        let urlString = "\(weatherURL)\(secureAPIkey)&q=\(cityName)\(units)"
         print(urlString)
     }
 }
