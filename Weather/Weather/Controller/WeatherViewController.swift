87495b6ee6f640df4c8bd88c4b06a6825a6f8f48
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-02-08 11:08:25 +0100

    feat: fetch city name from searchBar

--- Weather/Weather/Controller/WeatherViewController.swift
+++ Weather/Weather/Controller/WeatherViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 
 class WeatherViewController: UIViewController, UISearchBarDelegate {
-
+    
     @IBOutlet weak var cityLbl: UILabel!
     @IBOutlet weak var temperatureLbl: UILabel!
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +22,23 @@ class WeatherViewController: UIViewController, UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-           searchBar.endEditing(true)
-       }
+        searchBar.endEditing(true)
+    }
     
     func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
-         if searchBar.text != "" {
-             return true
-         } else {
-             searchBar.placeholder = "Type something..."
-             return false
-         }
-     }
+        if searchBar.text != "" {
+            return true
+        } else {
+            searchBar.placeholder = "Type something..."
+            return false
+        }
+    }
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-          cityLbl.text = searchBar.text
-          searchBar.text = ""
-      }
+        if let city = searchBar.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchBar.text = ""
+    }
 }
 
