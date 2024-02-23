ddaff8ec506e9f37015af0f3df399c41d52f3056
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-02-23 15:49:14 +0100

    feat: add WeatherModel + conditionName refactor

---
+++ Weather/Weather/Model/WeatherModel.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherModel.swift
+//  Weather
+//
+//  Created by Michal Pietrzak on 23/02/2024.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt.rain"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+           return "cloud"
+        default:
+            return "cloud.sun"
+        }
+    }
+}
