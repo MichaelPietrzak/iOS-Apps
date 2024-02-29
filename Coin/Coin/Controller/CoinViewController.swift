a623b5a97b557e43104afe19b60b43eee752edad
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-02-29 16:47:54 +0100

    feat: add data source for picker view + adopt protocol

--- Coin/Coin/Controller/CoinViewController.swift
+++ Coin/Coin/Controller/CoinViewController.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-class CoinViewController: UIViewController {
+class CoinViewController: UIViewController, UIPickerViewDataSource {
 
     @IBOutlet weak var priceView: UIView!
     @IBOutlet weak var currencyPicker: UIPickerView!
     @IBOutlet weak var currencyLbl: UILabel!
     @IBOutlet weak var coinLbl: UILabel!
     
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        currencyPicker.dataSource = self
+        
+        layoutUI()
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func layoutUI() {
+        priceView.layer.cornerRadius = 10
+    }
 }
--- Coin/Coin/Model/CoinManager.swift
+++ Coin/Coin/Model/CoinManager.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct CoinManager {
-
+    let currencyArray: [String] = [] // TODO: - add currencies
 }
