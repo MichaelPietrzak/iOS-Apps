17a2e65c29ef40f737e9f9b3a280fdaf798d8d78
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-03-01 14:33:22 +0100

    feat: secure coin API key using environment variable

--- Coin/Coin/Model/CoinManager.swift
+++ Coin/Coin/Model/CoinManager.swift
@@ -8,10 +8,13 @@
 import Foundation
 
 struct CoinManager {
+    
+    let secureAPIKey = ProcessInfo.processInfo.environment["YOUR_API_KEY"] ?? "Invalid API Key"
+    let coinURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     func getCoinPrice(for currency: String) {
         print(currency)
     }
 }
- 
