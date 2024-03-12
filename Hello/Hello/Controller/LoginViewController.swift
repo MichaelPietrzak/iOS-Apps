acd787cfa2bd50e0da2bd978466c75541ca36b75
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-03-12 14:59:15 +0100

    feat: implement login + error validation label

--- Hello/Hello/Controller/LoginViewController.swift
+++ Hello/Hello/Controller/LoginViewController.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func signInPressed(_ sender: UIButton) {
-        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorLabel.text = e.localizedDescription
+                } else {
+                    if let chatVC = self.storyboard?.instantiateViewController(withIdentifier: "chatViewController") {
+                        self.navigationController?.pushViewController(chatVC, animated: true)
+                    }
+                }
+            }
+        }
     }
 }
