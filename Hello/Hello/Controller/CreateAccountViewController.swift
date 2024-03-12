cee84e8eb5208a23eeb0d8fdbe3b44648fa1e1e0
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-03-12 14:13:16 +0100

    feat: implement new account + error validation label

--- Hello/Hello/Controller/CreateAccountViewController.swift
+++ Hello/Hello/Controller/CreateAccountViewController.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CreateAccountViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
     @IBAction func createAccountPressed(_ sender: UIButton) {
-        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorLabel.text = e.localizedDescription
+                } else {
+                    if let chatVC = self.storyboard?.instantiateViewController(withIdentifier: "chatViewController") {
+                        self.navigationController?.pushViewController(chatVC, animated: true)
+                    }
+                }
+            }
+        }
     }
 }
