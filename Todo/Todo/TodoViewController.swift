41ae8771aa160856acc5f33583d1fd1b1a1fcb34
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-03-15 14:39:50 +0100

    feat: populate tableView cells with sample data

--- Todo/Todo/TodoViewController.swift
+++ Todo/Todo/TodoViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class TodoViewController: UITableViewController {
-
+    
+    var itemArr = ["Buy apples", "Finish assignment", "Clean the bathroom"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArr.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArr[indexPath.row]
+        
+        return cell
     }
 }
 
