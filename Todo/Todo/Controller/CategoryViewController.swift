de491cae11886eabd261dc9c86e0efaf243d31bb
Michał Pietrzak <michal.pietrzak18@gmail.com>
2024-03-22 10:40:24 +0100

    feat: create + setup of CategoryView in storyboard

---
+++ Todo/Todo/Controller/CategoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryViewController.swift
+//  Todo
+//
+//  Created by Michal Pietrzak on 22/03/2024.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+}
